51fa76e7e0b8acbc21abb833cd757d3e16d1af8b
unknown <tareqrobin57@gmail.com>
2022-10-08 23:57:05 +0600

    product calc 1st stage done

--- smart-shop-online/index.js
+++ smart-shop-online/index.js
@@ -7,19 +7,37 @@ fetch('https://fakestoreapi.com/products').then(response => response.json()).the
             <div class="card prod-card col mt-4">
                 <img src= ${prod.image} height="100px" width="100px">
                 <div class="card-body">
-                    <h5 class="card-title">${prod.title.slice(0,10)}</h5>
+                    <h5 class="card-title">${prod.title.slice(0, 10)}</h5>
                     <p class="card-text">$<strong class="prod-one">${prod.price}</strong></p>
+                    <p class="d-none">shipping <input id="shipping-cost">50</input></p>
                 </div>
                 <div class="  prod-btn border-0">
-                    <button class="btn btn-primary w-100" type="button">Buy Now</button>
+                    <button class="btn btn-primary w-100" type="button" onClick="addToCart(${prod.id})">Add To Cart</button>
                 </div>
             </div>
             `;
         products.innerHTML = items;
 
     })
+})
 
+let price = 0;
+let shipping = 0;
+let totalPrice = 0;
+let addToCart = (props) => {
+    product.map((element) => {
+        if (element.id === props) {
+            price += parseFloat(element.price);
+            document.getElementById("price-input").value = price.toFixed(2);
+            shipping += 50;
+            document.getElementById("shipping").value = shipping.toFixed(2);
+            // vat += parseFloat(element.vat);
+            let vat = price * 5 / 100;
+            document.getElementById("vat").value = vat.toFixed(2);
+            totalPrice = parseFloat(price + shipping + vat);
+            document.getElementById("total-input").value = totalPrice.toFixed(2);
+        }
 
-
+    })
 }
-)
+
