c8a322abb95443628a0a4b53ab803422634601fc
unknown <tareqrobin57@gmail.com>
2022-10-10 12:44:29 +0600

    product my card updated, carousel updated, navbar updated

--- smart-shop-online/index.js
+++ smart-shop-online/index.js
@@ -2,9 +2,9 @@ let product = [];
 const products = document.getElementById('products');
 fetch('https://fakestoreapi.com/products').then(response => response.json()).then(data => {
     product = data;
-    data.slice(9, 18).map(prod => {
+    product.slice(9, 18).map(prod => {
         const items = products.innerHTML + `
-            <div class="card prod-card col mt-4">
+            <div class="card prod-card mt-4">
                 <img src= ${prod.image} height="100px" width="100px">
                 <div class="card-body">
                     <h5 class="card-title">${prod.title.slice(0, 10)}</h5>
@@ -12,7 +12,7 @@ fetch('https://fakestoreapi.com/products').then(response => response.json()).the
                     <p class="d-none">shipping <input id="shipping-cost">50</input></p>
                 </div>
                 <div class="  prod-btn border-0">
-                    <button class="btn btn-primary w-100" type="button" onClick="addToCart(${prod.id})">Add To Cart</button>
+                    <button class="btn btn-danger w-100" type="button" onClick="addToCart(${prod.id})">Add To Cart</button>
                 </div>
             </div>
             `;
@@ -21,23 +21,56 @@ fetch('https://fakestoreapi.com/products').then(response => response.json()).the
     })
 })
 
+let totalProducts = 0;
 let price = 0;
 let shipping = 0;
 let totalPrice = 0;
 let addToCart = (props) => {
     product.map((element) => {
         if (element.id === props) {
+            totalProducts += 1;
+            document.getElementById("total-products").value = totalProducts;
             price += parseFloat(element.price);
             document.getElementById("price-input").value = price.toFixed(2);
-            shipping += 50;
+            if (element.price > 500 && element.price < 800) {
+                shipping += 100;
+            }
+            else if (element.price > 800 && element.price < 1000) {
+                shipping += 150;
+            }
+            else if (element.price > 1000) {
+                shipping += 200;
+            }
             document.getElementById("shipping").value = shipping.toFixed(2);
-            // vat += parseFloat(element.vat);
-            let vat = price * 5 / 100;
-            document.getElementById("vat").value = vat.toFixed(2);
-            totalPrice = parseFloat(price + shipping + vat);
+            totalPrice = parseFloat(price + shipping);
             document.getElementById("total-input").value = totalPrice.toFixed(2);
+            let vat = price * 15 / 100;
+            document.getElementById("vat").value = vat.toFixed(2);
+            totalPriceNtax = parseFloat(price + shipping + vat);
+            document.getElementById("total-input-Ntax").value = totalPriceNtax.toFixed(2);
         }
 
     })
 }
 
+function checkout() {
+
+    if (totalPrice > 0) {
+        Swal.fire({
+            title: 'Order',
+            html: `Your total Price is ${totalPrice.toFixed(2)}`,
+            icon: 'success',
+            confirmButtonText: 'Confirm'
+        })
+    }
+    else {
+        Swal.fire({
+            tittle: 'Error',
+            text: 'Please select some items',
+            icon: 'error',
+            confirmButtonText: 'Ok'
+        })
+        return;
+
+    }
+}
